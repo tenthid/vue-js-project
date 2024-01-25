47b395bb3a882d99ab80f76e931abf2495e028ad
tenthid <tenthsut@gmail.com>
2024-01-25 14:25:37 +0800

    modal search height revision

--- src/components/modal/SearchModal.vue
+++ src/components/modal/SearchModal.vue
@@ -41,7 +41,7 @@
     bottom: 0;
     background-color: rgba(0, 0, 0, 0.5);
     width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     /* align-items: center; */
